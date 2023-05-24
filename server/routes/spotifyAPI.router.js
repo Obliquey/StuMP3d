1615a7ef59356b918e3e9fbac06d4f5d7d2bde1b
Anders Boyum <Acboyum@gmail.com>
2023-05-24 14:06:11 -0500

    Got our preview URLS! Our call for the tokens works, the API call flow to get to the preview URLS works, and we have successfully extracted them along with our album info

--- server/routes/spotifyAPI.router.js
+++ server/routes/spotifyAPI.router.js
@@ -1,54 +1,21 @@
 const axios = require('axios');
 const express = require('express');
-
-const querystring = require('querystring');
 const router = express.Router();
+const pool = require('../modules/pool');
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+const userStrategy = require('../strategies/user.strategy');
 require('dotenv').config();
-// const { oAuth } = require('../modules/OAuth-middlware')
 
 
 // vvvv all my info for spotify API calls vvvv
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-// vvvv stuff specifically for logging the user in vvvv
-const redirect_uri = 'http://localhost:5000/callback'
-const stateKey = 'spotify_auth_state';
 // function to generate random numbers to select random album/songs, etc
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
-  }
-
-const generateRandomString = function(length) {
-  let text = '';
-  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
-// generate random string func ^^^ and then call it to create a STATE that we will use in our authorization call and Spotify will use to check later vvvv
-const state = generateRandomString(16);
-
-
-// This is our call to Spotify to authorize the app with the user
-router.get('/login', (req, res) => {
-  console.log("In /spotify/login, here's hoping");
-  const scope = 'streaming user-read-playback-position';
-  // not sure what these are vvv but they seem to be necessary
-  res.cookie(stateKey, state);
-
-  // this redirects (imagine that) the user to a Spotify page where they can log in
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: CLIENT_ID,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
-})
-
+  };
 
 
 let config = {
@@ -60,9 +27,6 @@ let config = {
       'Cookie': '__Host-device_id=AQAoZkeT-nU1YJnRR7LdXXJnifEI05WNfTIoJvft1xaOw-JlsMQcGH46R5HzBZ0HiMwK5mTJ_B6uHztZEgfwNfN43UJ_sMlh_Es; sp_tr=false'
     }
   };
-  
-
-
 
 
 
@@ -70,45 +34,68 @@ let config = {
 // Also refactor everything out to 
   router.get('/:artist', (req, res) => {
     const artist = req.params.artist;
-    // first, gotta get our access token from Spotify
-    console.log("Got our artist:", artist);
-    let token;
-    axios.request(config)
-    // first .then response for this axios is the access token
-    .then((response) => {
-        token = response.data.access_token;
+    const userID = req.user.id;
+  
+    // fetch our token data from the DB
+    pool.query(`SELECT access_token, token_expires, refresh_token FROM "users" WHERE users.id = $1;`, [userID])
+    .then((dbRes) => {
+      // extract it
+        const token = dbRes.rows[0].access_token
+        const expiry = dbRes.rows[0].token_expires
+        const refreshToken = dbRes.rows[0].refresh_token
         let items = [];
 
-        // then, use the access token to make a request for artist info
-        return axios({
+        // Then, check if access_token is expired. If it is, we will need to request a new one.
+        if(expiry >= Date.now()) {
+          // gotta make a new request with the refresh token here, or redirect to a different function to refresh the token.
+          console.log("Gotta get a new token!", expiry);
+        } else if (expiry < Date.now()) {
+          console.log("Token is all good! Ask away");
+
+          return axios({
             method: 'GET',
             url: `https://api.spotify.com/v1/search?q=${artist}&type=album&include_external=audio&limit=5`,
             headers: { 
                 'Authorization': `Bearer  ${token}`
             }
+        }).then((response) => {
+          let rndmNum = getRndInteger(0, response.data.albums.items.length)
+          const albumID = response.data.albums.items[rndmNum].id;
+          const coverArt = response.data.albums.items[rndmNum].images[1];
+          const releaseDate = response.data.albums.items[rndmNum].release_date;
+          const albumName = response.data.albums.items[rndmNum].name;
+          const albumInfo = {
+            albumID: albumID,
+            coverArt: coverArt,
+            releaseDate: releaseDate,
+            albumName: albumName
+          }
+          console.log("album", albumInfo)
+  
+          return axios({
+              method: 'GET',
+              url: `https://api.spotify.com/v1/albums/${albumID}/tracks`,
+              headers: {
+                  'Authorization': `Bearer  ${token}`
+              }
+          }).then(response => {
+            let previewURLS = response.data.items.map(item => {
+              return {URL: item.preview_url, name: item.name};
+            })
+            console.log("Here's our response?:", previewURLS);
+            res.send(albumInfo, previewURLS);
+          }).catch(err => {
+            console.log("Error making GET req to Spotify for albums");
+          })
         }).catch(err => {
-            console.log("Something went horribly wrong", err);
+          console.log("Error making GET req to Spotify for artist", err);
         })
+      }}).catch(dbErr => {
+        console.log("Error connecting to DB:", dbErr);
+      })
         // second .then res is the return of ^^^^^ this second axios call, which are all the items from Spotify. Now we send those back to the client vvvvvv
-    }).then(response => {
-        // getting the ID of a random album from the response, to be used in another query
-        const albumToGet = response.data.albums.items[getRndInteger(0, response.data.albums.items.length)].id;
-        console.log("album", response.data.albums.items)
-
-        return axios({
-            method: 'GET',
-            url: `https://api.spotify.com/v1/albums/${albumToGet}/tracks`,
-            headers: {
-                'Authorization': `Bearer  ${token}`
-            }
-        })
-    }).then(response => {
-        console.log("Not sure what to call this:", response.data)
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-  })
+      
+  });
 
 
 module.exports = router;
