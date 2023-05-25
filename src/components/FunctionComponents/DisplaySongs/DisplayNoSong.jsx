eab4b7bcd6e1e45e6c4bcd80dff3df1035599baa
Anders Boyum <Acboyum@gmail.com>
2023-05-25 15:17:10 -0500

    Got my songs to display! It isn't pretty, but it's there and that's what matters

---
+++ src/components/FunctionComponents/DisplaySongs/DisplayNoSong.jsx
@@ -0,0 +1,8 @@
+
+// *This function is literally just a placeholder for our conditional render, until we get a song. Then DisplaySong will be rendered.
+function DisplayNoSong() {
+    return(
+        <p>Waiting for song</p>
+    )
+}
+export default DisplayNoSong;
--- src/components/FunctionComponents/DisplaySongs/DisplaySongs.jsx
+++ src/components/FunctionComponents/DisplaySongs/DisplaySongs.jsx
@@ -1,26 +1,27 @@
 import { useSelector, useDispatch } from "react-redux";
+import DisplayNoSong from "./DisplayNoSong";
 
 // * This component function will be called in the Play Page. It's job is to display the media player (preview url)
 // * and poooosibly display the choices too
 function DisplaySongs() {
-    const previews = useSelector( store => store.spotify[1])
+    const songsArrived = useSelector(store => store.spotify.songsArrived)
+    const previews = useSelector(store => store.spotify.previews)
 
     // gotta pick a random song
     function getRndInteger(min, max) {
         return Math.floor(Math.random() * (max - min) ) + min;
       };
 
-    let song = previews[getRndInteger(0, previews.length)]
+      let song = previews[getRndInteger(0, previews.length)]
+    if(song) {
+        console.log("Here's our song:",song);
+        return (
+            <embed className="ml-200" src={song.URL}/>
+        )
+    } else {
+        return <DisplayNoSong />
+    }
 
-    return(
-        <div>
-            {
-                <>
-                    if()
-                </>
-            }
-        </div>
-    )
 }
 
 export default DisplaySongs;
